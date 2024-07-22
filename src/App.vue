53f43b45efcd6018468073ce58fcbeb5202515c3
jrussellnj <joseph@thegreengrass.net>
2024-07-22 10:17:10 -0400

    Adding axios for API calls to OpenWeather proxy URL

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from 'axios';
 import SiteHeader from './components/SiteHeader.vue';
 import SiteFooter from './components/SiteFooter.vue';
 
@@ -7,6 +8,38 @@ export default {
   components: {
     SiteHeader,
     SiteFooter
+  },
+  data() {
+    return {
+      locationData: {
+        'latitude': '39.950802',
+        'longitude': '-75.160118'
+      },
+      weatherData: {},
+      weatherUnits: 'imperial'
+    }
+  },
+  created() {
+    this.getWeatherData();
+  },
+  methods: {
+    getWeatherData() {
+      console.log("getting weather data", this.locationData);
+
+      const url = `http://localhost:3000/weather?latitude=${this.locationData.latitude}&longitude=${this.locationData.longitude}&units=${this.weatherUnits}`;
+      console.log(url);
+      if (Object.keys(this.locationData).length) {
+        axios
+          .get(url)
+          .then((response) => {
+            this.weatherData = response.data;
+            console.log(this.weatherData);
+          });
+      }
+      else {
+        console.log("No location!");
+      }
+    }
   }
 }
 </script>
