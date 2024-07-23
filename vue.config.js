688fbc93eca05991b9f2f3943cbbed5b7bdb7988
jrussellnj <joseph@thegreengrass.net>
2024-07-23 11:30:54 -0400

    Adding a custom title to the app entry page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: "What's the dew point?",
+    },
+  },
   transpileDependencies: true
 })
