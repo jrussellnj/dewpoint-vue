452ef00cf5d116690dd145e4d4b38dd61ef78bba
jrussellnj <joseph@thegreengrass.net>
2024-07-23 22:16:35 -0400

    Adding icon attribution to the footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,7 +2,7 @@
   <footer class="text-center mt-4 text-sm">
     <a class="underline" target="_blank" rel="noopener noreferrer" href="https://www.weather.gov/arx/why_dewpoint_vs_humidity">What is the "dew point"?</a>
     <p>
-      Powered by OpenWeather API.
+      Powered by OpenWeather API. <a href="https://www.flaticon.com/free-icons/rain" title="rain icons">Rain icons created by riajulislam - Flaticon</a>. <a href="https://www.flaticon.com/free-icons/weather" title="weather icons">Weather icons created by hirschwolf - Flaticon</a>.
     </p>
   </footer>
 </template>
